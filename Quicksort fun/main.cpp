b61c65b1db14e48a58a280c2c210a11961f3094f
Sheaim <209226@pwr.edu.pl>
2015-03-26 11:43:24 +0100

    Quicksort
    Work in progress

---
+++ Quicksort fun/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <windows.h>
+#include <fstream>
+
+#include "Lista.hh"
+#include "Stos.hh"
+#include "Kolejka.hh"
+#include "Stoper.hh"
+
+/*!
+ *\mainpage Program framework benchmarkujacy dla struktury danych Stos
+ *\author Wojciech Makuch
+ *\date 16.03.2015
+ *\version 1.1
+ *Program przeprowadza operacje zliczania czasu wypelnienia struktury danych stos
+ *liczbami pseldolosowymi. Program sprawdza dzialanie i szybkosc obliczeniowa struktury
+ *danych przy alokacji pamieci zarowno o 1 element i razy 200%.
+ *Wyliczony czas podawany z dokladnoscia do us.
+ *Uzyskane dane program zapisuje do pliku o nazwie "Pomiar_czasu3.txt"
+ */
+
+using namespace std;
+
+int main() {
+
+    const char* nazwa_pliku="Pomiar_czasu3.txt";
+    fstream plik;
+    plik.open(nazwa_pliku,ios::out);
+    if(!plik.good())
+    {
+        cerr << "Blad otwarcia pliku." << endl;
+        return -1;
+    }
+    Stos<int> s;
+    int n=10;
+    cout << "Quicksort" << endl;
+    plik << "ilosc element.\tczas" << endl;
+    licz(s, n);
+    plik.close();
+    cout << "Zapisano." << endl;
+    system("PAUSE");
+    return 0;
+}
