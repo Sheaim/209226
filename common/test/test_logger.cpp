4f11c3cb32bce353feabf319623c621018dc02c5
Sheaim <209226@pwr.edu.pl>
2015-04-05 19:38:13 +0200

    Cleanum and rebranching of the files to Lab04 after pulling from external source (topeerz)

---
+++ common/test/test_logger.cpp
@@ -0,0 +1,44 @@
+/*
+ * test_logger.cpp
+ *
+ *  Created on: Apr 5, 2015
+ *      Author: topeerz
+ */
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include <string>
+#include <fstream>
+
+#include "../catch.hpp"
+#include "../logger.h"
+
+TEST_CASE( "logger logSimpleString", "[factorial]" ) {
+
+	// pre
+	const std::string logFileName = "lolz.txt";
+	student::common::Logger* pLogger = new student::common::Logger(logFileName);
+
+	// given
+	std::string logEntry = "tralalala";
+
+	// when
+	pLogger->log(logEntry);
+	delete pLogger;
+
+	std::string logFileContent;
+
+	std::fstream logfile;
+
+	logfile.open(logFileName, std::fstream::in);
+	logfile >> logFileContent;
+
+	logfile.close();
+
+	// then
+	REQUIRE(logFileContent == logEntry);
+
+	//cleanup
+	remove("lolz.txt");
+
+}
---
+++ quicksort/quicksort.cpp
@@ -0,0 +1,18 @@
+/*
+ * quicksort.cpp
+ * created on april 5 2015
+ * author: sheaim
+ *
+ */
+#include <iostream>
+#include "quicksort.h"
+
+void quicksort_number(int* collection)
+{
+
+
+}
+
+
+
+
