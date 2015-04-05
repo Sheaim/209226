46da893f295ccf880db8356842f691340113d7fc
Sheaim <209226@pwr.edu.pl>
2015-04-05 19:33:26 +0200

    Cleanup cmake files

--- quicksort/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/STUDIA/PAMiSI/209226/quicksort
-# Build directory: D:/STUDIA/PAMiSI/209226/quicksort
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(quicksortTest "D:/STUDIA/PAMiSI/209226/quicksort/quicksort_tests.exe")
-subdirs(test)
