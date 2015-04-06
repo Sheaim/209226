b26ac1324bf11562db8a067c5a6e15a28cf8bfb7
Sheaim <209226@pwr.edu.pl>
2015-04-06 17:00:45 +0200

    First attempt of universal template quicksort.

--- quicksort/quicksort.cpp
+++ quicksort/quicksort.cpp
@@ -10,7 +10,7 @@
 
 const std::string wrong_value_error_message = "Error: array part is not a number.";
 
-int* merge_arrays(int* low, int lowArrayLenght, int* high, int highArrayLenght)
+int* merge_arrays_number(int* low, int lowArrayLenght, int* high, int highArrayLenght)
 {
     std::cout<<"low array"<<std::endl;
     for(int i=0; i<lowArrayLenght;++i)
@@ -41,6 +41,7 @@ int* merge_arrays(int* low, int lowArrayLenght, int* high, int highArrayLenght)
     std::cout<<std::endl;
     return mergedArray;
 }
+
 
 int* quicksort_number(int* collection, int array_lenght)
 {
@@ -99,6 +100,95 @@ int* quicksort_number(int* collection, int array_lenght)
     return collection;
 }
 
+template<typename type>
+type* merge_arrays(type* low, int lowArrayLenght, type* high, int highArrayLenght)
+{
+    std::cout<<"low array"<<std::endl;
+    for(int i=0; i<lowArrayLenght;++i)
+    {
+        std::cout<<low[i]<<" ";
+    }
+    std::cout<<std::endl;
+    int mergedArrayLenght = lowArrayLenght+highArrayLenght;
+    type* mergedArray = new type[mergedArrayLenght];
+    for(int i=0; i<lowArrayLenght;++i)
+    {
+        mergedArray[i] = low[i];
+    }
+    std::cout<<"high array"<<std::endl;
+    for(int i=0; i<highArrayLenght;++i)
+    {
+        std::cout<<high[i]<<" ";
+    }
+    std::cout<<std::endl;
+    for(int i=0; i<highArrayLenght;++i)
+    {
+        mergedArray[i+lowArrayLenght] = high[i];
+    }
+    for(int i=0; i<mergedArrayLenght;++i)
+    {
+        std::cout<<mergedArray[i]<<" ";
+    }
+    std::cout<<std::endl;
+    return mergedArray;
+}
 
+template<typename type>
+type* quicksort(type* collection, int array_lenght)
+{
+    type pivot;
+    type* low = new type[array_lenght];
+    type* high = new type[array_lenght];
+    int counterOfLowArrayElements=0;
+    int counterOfHighArrayElements=0;
+    for(int i=0; i<array_lenght;++i)
+    {
+        std::cout<<collection[i]<<" ";
+    }
+    std::cout<<std::endl;
+    std::cout<<"finding pivot"<<std::endl;
+    pivot = 0;
+    for (int i=0; i<array_lenght; ++i)
+    {
+        pivot = pivot + collection[i];
+    }
+    pivot = pivot/array_lenght;
+    for(int i=0; i<array_lenght; ++i)
+    {
+        if (collection[i]<=pivot)
+        {
+            std::cout<<"adding "<<collection[i]<<"to low"<<std::endl;
+            low[counterOfLowArrayElements]=collection[i];
+            ++counterOfLowArrayElements;
+        }
+        else
+            if(collection[i]>pivot)
+        {
+            high[counterOfHighArrayElements]=collection[i];
+            ++counterOfHighArrayElements;
+            std::cout<<"adding "<<collection[i]<<"to high"<<std::endl;
+        }
+        else
+        {
+            std::cout<<wrong_value_error_message<<std::endl;
+            break;
+        }
+    }
+
+    if(counterOfLowArrayElements!=1)
+    {
+        quicksort_number(low, counterOfLowArrayElements);
+        quicksort_number(high, counterOfHighArrayElements);
+    }
+
+
+    for(int i=0; i<array_lenght;++i)
+    {
+        collection[i] = merge_arrays<type>(low, counterOfLowArrayElements, high, counterOfHighArrayElements)[i];
+        std::cout<<collection[i]<<" ";
+    }
+    std::cout<<std::endl;
+    return collection;
+}
 
 
--- quicksort/quicksort.h
+++ quicksort/quicksort.h
@@ -17,8 +17,11 @@
  *
  */
 int* quicksort_number(int* collection,int array_lenght);
-int* merge_arrays(int* low, int lowLenght, int* high, int highLenght);
+int* merge_arrays_number(int* low, int lowLenght, int* high, int highLenght);
 
-
+template<typename type>
+type* quicksort(type* collection,int array_lenght);
+template<typename type>
+type* merge_arrays(type* low, int lowLenght, type* high, int highLenght);
 
 #endif // QUICKSORT_H_
