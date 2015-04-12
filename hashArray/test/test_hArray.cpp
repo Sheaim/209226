28d45d2146b18f0c67c4177846c207cc4af21ad2
Sheaim <209226@pwr.edu.pl>
2015-04-13 00:35:30 +0200

    Added basis for the hashing array

---
+++ hashArray/test/test_hArray.cpp
@@ -0,0 +1,21 @@
+
+/*
+ * test_hArray.cpp
+ *
+ *  Created on: Apr 5, 2015
+ *      Author: sheaim
+ */
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include <string>
+#include <fstream>
+
+#include "common/logger.h"
+#include "common/catch.hpp"
+#include "../hArray.h"
+
+TEST_CASE ("simple hashing array test", "[factorial]")
+{
+    REQUIRE(false);
+}
