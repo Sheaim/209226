cb7803322f1bab92d841fadc940a62476487af06
Sheaim <209226@pwr.edu.pl>
2015-05-20 18:16:39 +0200

    modified test file

--- AVL/test/test_AVL.cpp
+++ AVL/test/test_AVL.cpp
@@ -10,28 +10,53 @@
 #include <string>
 #include <fstream>
 
+#include <math.h>
 #include "common/logger.h"
 #include "common/catch.hpp"
 #include "../AVL.h"
+#include "common/observer.h"
 
 TEST_CASE( "AVL binary tree, simple test", "[factorial]" ) {
 
 	// pre
 	std::cout<<"abc"<<std::endl;
-    const std::string logFileName = "lolz.txt";
+    const std::string logFileName = "logfile.txt";
 	std::auto_ptr<student::common::Logger> pLogger(new student::common::Logger(logFileName));
+	Observer* obs = new Observer();
 	// given
 	AVLTree* testingTree = new AVLTree();
 	int n;
 	// when
-	for(int i=0;i<10;++i)
+	//adding elements
+	double meanAddTime = 0;
+	double meanSearchTime = 0;
+	for(int i=0;i<(1000);++i)
     {
-        n = rand()%10;
+        n = i;
+        obs->triggerTimer();
         testingTree->push(n);
+        obs->triggerTimer();
+        obs->logTimer();
+        meanAddTime = obs->giveTimerData()[0];
+        obs->reset();
         std::cout<<"\n\n\nnodes added"<<std::endl;
+        obs->triggerTimer();
+        //The algorithm will search for an element with the given number and return true should it find it.
+        testingTree->search(n);
+        obs->triggerTimer();
+        obs->logTimer();
+        meanSearchTime = obs->giveTimerData()[0];
+        obs->reset();
+        //searching
         testingTree->print(testingTree->getRoot(), 1);
+        pLogger->log("elements:");
+        pLogger->log(i);
+        pLogger->log("----AddTime----");
+        pLogger->log(meanAddTime);
+        pLogger->log("----SearchTime----");
+        pLogger->log(meanSearchTime);
+        pLogger->log("-------");
     }
-    delete testingTree;
 	// then
     REQUIRE(false);
 }
